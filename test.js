246d7d44db8c3e551cc7cabc303a5813c86a4391
UltimateProdigy <ayobamiakinola84@gmail.com>
2025-12-12 10:48:27 +0100

    fix: actions

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-console.log(setTimeout("test running", 3000));
-console.log("test completed")
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function runTest() {
+  console.log("test running");
+  await delay(3000);
+  console.log("test completed");
+}
+
+runTest();
